c296465da3f640ea628775b37c25d79b4b803fc8
FigMan57 <ian57anderson@gmail.com>
2019-04-21 20:32:07 -0400

    Graph fix

--- src/LoginController.java
+++ src/LoginController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+
 import java.io.IOException;
 import java.net.URL;
 
